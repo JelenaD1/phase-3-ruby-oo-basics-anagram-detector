3929818e5e21a2fe93cdd0948caa9024d52a451d
Jelena <jelena.durovic@powerhrg.com>
2021-08-09 09:48:57 -0400

    commit tree 6096ba1c2a91fd6a24508fb9f0bddc5c246652e0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end 
+
+    def match(array_of_anagrams)
+        array = []
+        word = @word.chars
+        array_of_anagrams.each do |string|
+            if  string.chars.sort == word.sort
+                array << string  
+           
+            end 
+        end 
+        array 
+
+    end 
+end
+
